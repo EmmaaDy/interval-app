5ece0ea30ed26f4cd7104224210c0b99ee0a4e17
EmmaaDy <emma.dybdorf@hotmail.com>
2024-10-25 14:43:33 +0200

    Add shaking animation to FaBell icon on alarm

--- src/components/AlarmView/AlarmView.js
+++ src/components/AlarmView/AlarmView.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { FaBell } from 'react-icons/fa';
+import { motion } from 'framer-motion'; 
 import './AlarmView.css';
 
 const AlarmView = ({ onResetAndGoBack }) => {
   return (
     <div className="alarm-view">
-    <div className="alarm-container">
-      <div className="alarm-icon">
-        <div className="ring-indicator left"></div> {/* Vänstra sträcket */}
-        <FaBell style={{ color: 'gray', fontSize: '100px' }} />
-        <div className="ring-indicator right"></div> {/* Högra sträcket */}
+      <div className="alarm-container">
+        <motion.div
+          className="alarm-icon"
+          animate={{ rotate: [0, 5, -5, 0] }} // Definiera skakningsanimation
+          transition={{ duration: 0.5, repeat: Infinity }} // Animationens längd och upprepning
+        >
+          <div className="ring-indicator left"></div>
+          <FaBell style={{ color: 'gray', fontSize: '100px' }} />
+          <div className="ring-indicator right"></div>
+        </motion.div>
+        <h2 className="alarm-text">Times Up!</h2>
+        <button className="common-button" onClick={onResetAndGoBack}>Set New Timer</button>
       </div>
-      <h2 className="alarm-text">Times Up!</h2>
-      <button className="common-button" onClick={onResetAndGoBack}>Set New Timer</button>
-    </div>
     </div>
   );
 };
